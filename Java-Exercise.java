56184314a3ee735ed5d63794564f73ce1da63d6c
Hellocat22 <riyakavajravesaka@gmail.com>
2025-04-30 12:53:18 +0800

    Hi

--- Java-Exercise.java
+++ Java-Exercise.java
@@ -2,6 +2,7 @@ public class FirstClass4Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01257169\"\n姓名：\"張欽平\"");
+        System.out.println("\"學號：\"01257168\"\n姓名：\"許家豪\"");
         
     }
 }
